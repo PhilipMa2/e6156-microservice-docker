81ad370dd1d8b8c01bef26bc3acc434dd796208a
PhilipMa2 <ma_philip2000@hotmail.com>
2023-12-20 16:40:58 -0500

    deploy

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
-import logging
-from logging.handlers import RotatingFileHandler
+from sqlalchemy import UniqueConstraint
 
 app = Flask(__name__)
 
@@ -10,28 +9,17 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
-handler = RotatingFileHandler('app.log', maxBytes=10000, backupCount=1)
-handler.setLevel(logging.INFO)
-app.logger.addHandler(handler)
-
-@app.before_request
-def log_request_info():
-    app.logger.info('Request Headers: %s', request.headers)
-    app.logger.info('Request Body: %s', request.get_data())
-
-@app.after_request
-def log_response_info(response):
-    app.logger.info('Response Status Code: %s', response.status_code)
-    app.logger.info('Response Body: %s', response.get_data())
-    return response 
-
 class Team(db.Model):
     __tablename__ = 'teams'
 
     team_id = db.Column(db.Integer, primary_key=True)
-    requester_id = db.Column(db.Integer, nullable=False)
-    requestee_id = db.Column(db.Integer, nullable=False)
+    requester_id = db.Column(db.VARCHAR(200), nullable=False)
+    requestee_id = db.Column(db.VARCHAR(200), nullable=False)
     confirmed = db.Column(db.Boolean, default=False)
+
+    __table_args__ = (
+        UniqueConstraint('requester_id', 'requestee_id', name='unique_request'),
+    )
 
 with app.app_context():
     db.create_all()
@@ -40,24 +28,69 @@ with app.app_context():
 def hello():
     return '<h1>Hello from flask</h1>'
 
-@app.route("/team/<int:student_id>", methods=["GET"])
-def read_team(student_id):
-    team = Team.query.filter((Team.requestee_id == student_id) | (Team.requestee_id == student_id)).first()
-    if team:
-        return jsonify(team.__dict__)
+@app.route("/team/applied/<int:student_id>", methods=["GET"])
+def read_team_applied(student_id):
+    teams = Team.query.filter(Team.requester_id == student_id, Team.confirmed.is_(False))
+    if teams:
+        teams_list = [
+            {
+                'team_id': team.team_id,
+                'requester_id': team.requester_id,
+                'requestee_id': team.requestee_id,
+                'confirmed': team.confirmed,
+            }
+            for team in teams
+        ]
+        return jsonify(teams_list)
     return jsonify({"message": "Team not found"}), 404
+
+@app.route("/team/received/<int:student_id>", methods=["GET"])
+def read_team_received(student_id):
+    teams = Team.query.filter(Team.requestee_id == student_id, Team.confirmed.is_(False))
+    if teams:
+        teams_list = [
+            {
+                'team_id': team.team_id,
+                'requester_id': team.requester_id,
+                'requestee_id': team.requestee_id,
+                'confirmed': team.confirmed,
+            }
+            for team in teams
+        ]
+        return jsonify(teams_list)
+    return jsonify({"message": "Team not found"}), 404
+
+@app.route("/team/formed/<int:student_id>", methods=["GET"])
+def read_team_formed(student_id):
+    teams = Team.query.filter((Team.requester_id == student_id) | (Team.requestee_id == student_id), Team.confirmed.is_(True))
+    if teams:
+        teams_list = [
+            {
+                'team_id': team.team_id,
+                'requester_id': team.requester_id,
+                'requestee_id': team.requestee_id,
+                'confirmed': team.confirmed,
+            }
+            for team in teams
+        ]
+        return jsonify(teams_list)
+    return jsonify({'message': 'Team not found'}), 404
 
 @app.route("/team", methods=["POST"])
 def create_team():
-    data = request.get_json()
-    new_team = Team(
-        requester_id=data.get("requester_id"),
-        requestee_id=data.get("requestee_id")
-    )
-    db.session.add(new_team)
-    db.session.commit()
+    try:
+        data = request.get_json()
+        new_team = Team(
+            requester_id=data.get("requester_id"),
+            requestee_id=data.get("requestee_id")
+        )
+        db.session.add(new_team)
+        db.session.commit()
 
-    return jsonify(new_team.__dict__), 201
+        return jsonify({'message': 'Team created successfully'}), 201
+    except Exception as e:
+        print(f"Error creating team: {str(e)}")
+        return jsonify({"error": "An error occurred during team creation"}), 500
 
 @app.route("/team/<int:team_id>", methods=["PUT"])
 def confirm_team(team_id):
